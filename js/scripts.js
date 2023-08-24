8611e87f6bb939fe30c693d48f6f17e60ac2573e
rushdihijab <103061146+rushdihijab@users.noreply.github.com>
2023-08-24 12:42:31 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,26 @@
-alert('be good')
+let pokemonList =[
+  {
+    name: "Bulbasaur",
+    height: 7,
+    types:['grass', 'poison']
+  },
+  {
+  name: "Ivysaur",
+  height: 10, 
+  types:['grass', 'poison']
+  },
+  {
+  name: "Venusaur", 
+  height: 20, 
+  types:['grass', 'poison']
+} 
+];
+let maxHeight = 15;
+  for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height > maxHeight) {document.write(`${pokemonList[i].name}  (height: ${pokemonList[i].height}) -wow , that big!`);
+  } else {
+    document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height})`);
+  }
+  }
+
+
